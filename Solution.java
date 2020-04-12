63b059e07ce5bdb89dfed861f264c2f4001982fe
miroleg <55798711+miroleg@users.noreply.github.com>
2020-04-12 22:58:43 +0700

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,72 @@
+package com.javarush.task.task21.task2107;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/* 
+Глубокое клонирование карты
+*/
+public class Solution implements Cloneable{
+    protected Map<String, User> users = new LinkedHashMap();
+
+
+
+    public Object clone() throws CloneNotSupportedException {
+        Solution solution = new Solution();
+        solution.users = new LinkedHashMap<>(this.users.size());
+        solution.users.putAll(this.users);
+        return solution;
+        
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.users.put("Hubert", new User(172, "Hubert"));
+        solution.users.put("Zapp", new User(41, "Zapp"));
+        Solution clone = null;
+        try {
+
+            clone = (Solution) solution.clone();
+            System.out.println(solution);
+            System.out.println(clone);
+
+            System.out.println(solution.users);
+            System.out.println(clone.users);
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+
+
+    public static class User implements Cloneable{
+        int age;
+        String name;
+
+
+
+        public  User(int age, String name)  {
+            this.age = age;
+            this.name = name;
+
+        }
+        public Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof User)) return false;
+            User user = (User) o;
+            return age == user.age &&
+                    Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(age, name);
+        }
+    }
+}
